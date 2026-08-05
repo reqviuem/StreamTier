211a699c8c12e896be171b29a8f547ad3a1e73a6
reqvi <slavil.larin@gmail.com>
2026-08-05 23:34:40 +0200

    Add initial database schema migration with Identity, Subscription, and Plan models, and seed plans data

---
+++ StreamTier.API/Migrations/20260804234623_InvoiceUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamTier.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InvoiceUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
