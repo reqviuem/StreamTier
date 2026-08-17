af4e062d2eaaa95b757131c1c34f6600ef84ae6f
reqvi <slavil.larin@gmail.com>
2026-08-17 02:24:23 +0200

    Update subscription plan seed data with new Stripe price IDs

---
+++ StreamTier.API/Migrations/20260816231617_UpdateSubscriptionPlanSeed.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamTier.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSubscriptionPlanSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "Id",
+                keyValue: "FreePlan",
+                column: "StripePriceId",
+                value: "prod_V5IHOhTATkiTMx");
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "Id",
+                keyValue: "PremiumPlan",
+                column: "StripePriceId",
+                value: "prod_V5IJ6UpKjjaG71");
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "Id",
+                keyValue: "StandardPlan",
+                column: "StripePriceId",
+                value: "prod_V5IIRM7Z8U74fE");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "Id",
+                keyValue: "FreePlan",
+                column: "StripePriceId",
+                value: "22");
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "Id",
+                keyValue: "PremiumPlan",
+                column: "StripePriceId",
+                value: "12");
+
+            migrationBuilder.UpdateData(
+                table: "Plans",
+                keyColumn: "Id",
+                keyValue: "StandardPlan",
+                column: "StripePriceId",
+                value: "38");
+        }
+    }
+}
