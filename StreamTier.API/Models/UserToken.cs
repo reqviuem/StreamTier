67be3869a035efb764982157f84e051264890615
reqvi <slavil.larin@gmail.com>
2026-09-19 16:59:37 +0200

    Add `UserToken` model with required properties (`Id`, `UserId`, `Token`, `ExpiresAt`)

---
+++ StreamTier.API/Models/UserToken.cs
@@ -0,0 +1,9 @@
+﻿namespace StreamTier.API.Models;
+
+public class UserToken
+{
+    public Guid Id { get; set; }
+    public required string UserId { get; set; }
+    public required string Token { get; set; }
+    public required DateTime ExpiresAt { get; set; }
+}
