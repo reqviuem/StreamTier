8148ed7bf8a41b2777da3442f0b4f5c9a5ee541a
reqvi <slavil.larin@gmail.com>
2026-09-23 03:00:21 +0200

    Add unique indexes for `StripeInvoiceId` and `StripeSubscriptionId` in `Invoices` and `Subscriptions` tables.

---
+++ StreamTier.API/Migrations/20260923004410_AddUniqueInvoiceIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamTier.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueInvoiceIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_StripeInvoiceId",
+                table: "Invoices",
+                column: "StripeInvoiceId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_StripeInvoiceId",
+                table: "Invoices");
+        }
+    }
+}
---
+++ StreamTier.API/Migrations/20260923004502_AddUniqueSubscriptionIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamTier.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueSubscriptionIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_StripeSubscriptionId",
+                table: "Subscriptions",
+                column: "StripeSubscriptionId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Subscriptions_StripeSubscriptionId",
+                table: "Subscriptions");
+        }
+    }
+}
